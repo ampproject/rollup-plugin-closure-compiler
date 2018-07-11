e9d55f38cbb45ce488467c9a41bef14459524a2b
Kristofer Baxter <kristofer@kristoferbaxter.com>
2018-07-10 19:45:02 -0700

    Use Rollup to bundle the plugin

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2018 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default {
+  input: './transpile/index.js',
+  external: ['util', 'fs', 'google-closure-compiler', 'temp-write'],
+  output: [
+    {
+      file: './dist/index.mjs',
+      format: 'es',
+    },
+    {
+      file: './dist/index.js',
+      format: 'cjs',
+    },
+  ],
+}
