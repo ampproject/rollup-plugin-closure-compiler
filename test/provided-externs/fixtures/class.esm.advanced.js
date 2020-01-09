7389fd75bbcf04672039251f7ae757ac7af3de05
Kristofer Baxter <kristofer@kristoferbaxter.com>
2020-01-08 17:40:12 -0800

    js match was too broad

--- test/provided-externs/fixtures/class.esm.advanced.js
+++ test/provided-externs/fixtures/class.esm.advanced.js
@@ -1,9 +1,1 @@
-class a {
-  constructor(b) {
-    this.a = b;
-  }
-  console() {
-    console.log(this.a);
-  }
-}
-export var ExportThis = a;
+class a{constructor(b){this.a=b}console(){console.log(this.a)}}export var ExportThis=a;
--- test/provided-externs/fixtures/class.esm.default.js
+++ test/provided-externs/fixtures/class.esm.default.js
@@ -1,9 +1,1 @@
-class a {
-  constructor(b) {
-    this.name_ = b;
-  }
-  console() {
-    console.log(this.name_);
-  }
-}
-export var ExportThis = a;
+class a{constructor(b){this.name_=b}console(){console.log(this.name_)}}export var ExportThis=a;
